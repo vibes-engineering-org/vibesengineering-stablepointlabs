436f953feefc65eec56b7bf7760f0bd853b3e1ef
vibesengineering <205912982+vibesengineering@users.noreply.github.com>
2025-09-17 12:43:59 +0000

    Code generation: d928f173

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const PROJECT_TITLE = "StablePointLabs";
-export const PROJECT_DESCRIPTION = "by @vibes.engineering";
+export const PROJECT_DESCRIPTION = "Advanced blockchain analytics and insights platform";
 export const PROJECT_CREATOR = "vibes.engineering";
 export const PROJECT_AVATAR_URL =
   "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/37460dae-996e-481a-9c2c-0374da719400/anim=false,fit=contain,f=auto,w=576";
